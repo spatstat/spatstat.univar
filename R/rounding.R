36bf7d77d782bbfb1a1ee9f045bb4745d0d47a22
Adrian Baddeley <Adrian.Baddeley@curtin.edu.au>
2024-06-14 16:00:51 +0800

    Add firstdigit, lastdigit etc

--- R/rounding.R
+++ R/rounding.R
@@ -1,6 +1,6 @@
 #'   detect rounding of data
 #'
-#'   $Revision: 1.1 $ $Date: 2024/06/12 06:23:36 $
+#'   $Revision: 1.4 $ $Date: 2024/06/14 05:20:33 $
 #'
 
 rounding <- function(x) {
@@ -16,7 +16,7 @@ rounding.default <- function(x) {
     k <- 0
     smallk <- -log10(.Machine$double.xmax)
     repeat {
-      if(k < smallk || !isTRUE(all.equal(x, round(x, k-1))))
+      if(k < smallk || !all(x == round(x, k-1)))
         return(k)
       k <- k-1
     }
@@ -25,9 +25,32 @@ rounding.default <- function(x) {
     k <- 1
     bigk <- -log10(.Machine$double.eps)
     repeat {
-      if(k > bigk || isTRUE(all.equal(x, round(x, k))))
+      if(k > bigk || all(x == round(x, k)))
         return(k)
       k <- k+1
     }
   }
 }
+
+
+## least significant digit in decimal representation
+lastdigit <- function(x) {
+  x <- abs(as.numeric(x))
+  z <- (x * 10^sapply(x, rounding.default)) %% 10
+  return(z)
+}
+
+## most significant digit in decimal representation
+firstdigit <- function(x) {
+  x <- abs(as.numeric(x))
+  z <- trunc(x/10^(floor(log10(ifelse(x == 0, 1, x)))))
+  return(z)
+}
+
+## number of digits in decimal representation
+ndigits <- function(x) {
+  x <- abs(as.numeric(x))
+  z <- pmax(1, ceiling(log10(ifelse(x == 0, 1, x * 10^sapply(x, rounding.default)))))
+  return(z)
+}
+
